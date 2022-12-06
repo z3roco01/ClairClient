21965f60ef309851a493319f7e5f2d58e9e95491
z3roco01 <zerocool@e.email>
2022-12-06 09:18:31 -0600

    Remove remappedSrc

--- remappedSrc/unknownuser2/clairclient/module/modules/FullBright.java
+++
@@ -1,9 +0,0 @@
-package unknownuser2.clairclient.module.modules;
-
-import unknownuser2.clairclient.module.Module;
-
-public class FullBright extends Module {
-    public FullBright() {
-        super("Full Bright", "The world is always max brightness.", "fbright");
-    }
-}
--- remappedSrc/unknownuser2/clairclient/module/modules/NoWeather.java
+++
@@ -1,9 +0,0 @@
-package unknownuser2.clairclient.module.modules;
-
-import unknownuser2.clairclient.module.Module;
-
-public class NoWeather extends Module {
-    public NoWeather() {
-        super("No weather", "Prevents weather from rendering.", "nweath");
-    }
-}
